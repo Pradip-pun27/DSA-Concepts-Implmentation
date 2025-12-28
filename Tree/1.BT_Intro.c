a861cf8d7a8867d99daf60230732937e991a9658
Pradip-pun27 <pradippoonmgr@gmail.com>
2025-12-28 21:58:01 +0545

    Tree update

--- Tree/1.BT_Intro.c
+++ Tree/1.BT_Intro.c
@@ -32,10 +32,33 @@ void Preorder(struct Node *root){
     Preorder(root->left);
     Preorder(root->right);
 }
+
+void Inorder(struct Node *root){
+    if(root == NULL){
+        return;
+    }
+    Preorder(root->left);
+    printf("%d ",root->data);
+    Preorder(root->right);
+}
+
+void Postorder(struct Node *root){
+    if(root == NULL){
+        return;
+    }
+    Preorder(root->left);
+    Preorder(root->right);
+    printf("%d ",root->data);
+}
+
 struct Node *root = NULL;
 int main() {
     root = Create();
-    printf("By Traversing the Binary Tree(data items are)\n");
+    printf("\nThe Preorder traversal of a B.T is\n");
     Preorder(root);
+    printf("\nThe Inororder traversal of a B.T is\n");
+    Inorder(root);
+    printf("\nThe Postorder traversal of a B.T is\n");
+    Postorder(root);
  return 0;
 }
